abfd2433f62901a40368da5638b6c97b7db4655f
Clarissa Ramos <angie.ramos20137@gmail.com>
2025-12-05 09:01:53 -0600

    clase entry

---
+++ LabPro2#7_Hasp/src/labpro2/pkg7_hasp/Entry.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labpro2.pkg7_hasp;
+
+/**
+ *
+ * @author HP
+ */
+public class Entry {
+    private String username;
+    private long posicion;
+    private Entry siguiente;
+    
+    public Entry(String username, long posicion){
+        this.username = username;
+        this.posicion = posicion;
+        this.siguiente = null;
+    }
+    
+    public String getUsername(){
+        return username;
+    }
+    
+    public long getPosicion(){
+        return posicion;
+    }
+    
+    public Entry getSiguiente(){
+        return siguiente;
+    }
+    
+    public void setSiguiente(Entry siguiente){
+        this.siguiente = siguiente;
+    }
+}
